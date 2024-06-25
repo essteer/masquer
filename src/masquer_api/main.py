3b0fa0c2cc48bc8081b65fb4d35357ed29460961
essteer <essteer@pm.me>
2024-06-25 17:30:04 +0800

    docs: Update API docs

--- src/masquer_api/main.py
+++ src/masquer_api/main.py
@@ -6,13 +6,13 @@ from masquer.__about__ import __version__
 
 
 DESCRIPTION = """
-Use `masquer API` to obtain any combination of a random user-agent, referer or header data template, then use this with a library like [`requests`](https://github.com/psf/requests) to control the session data you send to other services.
+Use `masquer` to obtain any combination of a random user-agent, referer or header data template, then use this with a library like [`requests`](https://github.com/psf/requests) to control the session data you send to other services.
 
 The user-agent data is drawn from [this list](https://www.useragents.me/) of the most common desktop user-agents, and referer data is taken from [this list](https://gs.statcounter.com/search-engine-market-share/desktop/worldwide) of search engines with the largest global market share.
 
 Weighted random selections are made from those lists to approximate authentic header data patterns.
 
-A basic header template with common attributes — including the recommended [`"Upgrade-Insecure-Requests": "1"`](https://stackoverflow.com/questions/31950470/what-is-the-upgrade-insecure-requests-http-header/32003517#32003517) — is also provided and defaults to the most common referer and user-agent data from the above lists.
+A basic header template with common attributes — like [`"Upgrade-Insecure-Requests": "1"`](https://stackoverflow.com/questions/31950470/what-is-the-upgrade-insecure-requests-http-header/32003517#32003517) — is also provided and defaults to the most common referer and user-agent data from the above lists.
 """
 
 VERSION = __version__
