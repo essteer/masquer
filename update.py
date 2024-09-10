65568f998f15f5159dee7468bedec6b1bc3c1fa9
essteer <essteer@pm.me>
2024-09-10 21:58:29 +0100

    refactor: Remove comments and edit docstrings

--- update.py
+++ update.py
@@ -4,25 +4,19 @@ import requests
 from bs4 import BeautifulSoup
 
 
-# Get absolute path for root directory
 ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
-# Path to assets and utils directories
 ASSETS_DIR = os.path.join(ROOT_DIR, "assets")
 UTILS_DIR = os.path.join(ROOT_DIR, "src", "masquer", "utils")
 
 
 def update_useragents() -> bool:
-    """
-    Gets latest useragent stats
-    Saves stats to JSON file
-    """
+    """Gets latest user-agent stats and saves them to JSON file"""
     try:
         response = requests.get("https://www.useragents.me/")
         html = BeautifulSoup(response.content, "html.parser")
-        # Find div for desktop stats
         div = html.find("div", id="most-common-desktop-useragents-json-csv")
         textarea = div.find(string="JSON").find_next("textarea").contents[0]
-        # Convert to JSON and save
+
         json_string = json.loads(textarea)
         with open(os.path.join(ASSETS_DIR, "useragents.json"), "w") as f:
             json.dump(json_string, f)
@@ -35,10 +29,7 @@ def update_useragents() -> bool:
 
 
 def update_referers() -> bool:
-    """
-    Gets latest referer stats
-    Saves stats to JSON file
-    """
+    """Gets latest referer stats and saves them to JSON file"""
     SITES = {
         "baidu": "https://www.baidu.com",
         "bing": "https://bing.com",
@@ -58,17 +49,12 @@ def update_referers() -> bool:
 
         search_engines = []
         market_shares = []
-        # Loop through <tbody> rows
+
         for row in tbody.find_all("tr"):
-            # Find <th> element (search engine name)
             search_engine = row.find("th")
-            # Find <span> element with class "count" (market share)
             market_share = row.find("span", class_="count")
-            # Confirm both elements found
             if search_engine and market_share:
-                # Extract text content from <th> element
                 search_engines.append(search_engine.text.strip().lower())
-                # Convert market share text to float
                 market_share_text = market_share.text.strip()
                 market_shares.append(float(market_share_text))
 
@@ -113,7 +99,6 @@ def update_assets() -> bool:
     Returns True if successful else False
     """
     try:
-        # Get JSON file data
         header_data = extract_data(os.path.join(ASSETS_DIR, "header.json"))
         referer_data = extract_data(os.path.join(ASSETS_DIR, "referers.json"))
         useragent_data = extract_data(os.path.join(ASSETS_DIR, "useragents.json"))
@@ -124,7 +109,6 @@ def update_assets() -> bool:
         useragents = [obj["ua"] for obj in useragent_data]
         useragent_weights = [obj["pct"] for obj in useragent_data]
 
-        # Save JSON content to assets.py
         with open(os.path.join(UTILS_DIR, "assets.py"), "a") as f:
             f.seek(0)
             f.truncate()
