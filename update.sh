0edc1c9afb8aaf3751e74f1b3781345a96c005f4
Elliott Steer <117514332+essteer@users.noreply.github.com>
2024-09-10 22:01:44 +0100

    build: Update docs for new uv build features (#12)
    * build: Move pyproject.toml optional dependencies to tool.uv.dev-dependencies and generate uv.lock file
    
    * refactor: Remove excess comments
    
    * refactor: Remove comments and edit docstrings

--- update.sh
+++ update.sh
@@ -1,33 +1,20 @@
 #!/bin/bash
 
-######################################################
-# update.sh
-######################################################
-
-# This version 23 June 2024
-
 # Activates venv and runs ./update.py 
 # to get and save latest assets
 
-######################################################
-
-# Activate virtual environment
 source .venv/bin/activate
 
-# Check whether virtual environment is activated
 if ! source .venv/bin/activate; then
   echo "Error: couldn't activate venv."
   echo "Create venv or update this script's venv path."
-  exit 1  # Exit with code 1 to indicate an error
+  exit 1
 fi
 
-# Run update.py
 python3 update.py
 
-# Check the exit code of update.py
 if [[ $? -eq 1 ]]; then
   echo "Update failed — ensure BeautifulSoup4 is installed."
 fi
 
-# Deactivate virtual environment
 deactivate
