fde063890db540be9fd7e5fc47d703d6d276201e
aabdulbasset <aabdulbasset5@gmail.com>
2024-09-28 08:53:34 +0300

    [feat] add v1,v0 routes [feat] add count param

--- src/api/routes.py
+++ src/api/routes.py
@@ -11,14 +11,29 @@ router = APIRouter()
 
 
 @router.get("/masq")  # maintain for backwards compatibility
-@router.get("/api/v1/masq")
-def get_masq(
+@router.get("/api/v0/masq")
+def get_masq_v0(
     ua: Union[bool, None] = True,
     rf: Union[bool, None] = False,
     hd: Union[bool, None] = False,
 ):
     logger.info(f"Request: [{ua=} {rf=} {hd=}]")
     response = masq(ua, rf, hd)
+    logger.info(f"Response: [{response}]")
+
+    return JSONResponse(content=response)
+
+@router.get("/api/v1/masq")
+def get_masq(
+    ua: Union[bool, None] = True,
+    rf: Union[bool, None] = False,
+    hd: Union[bool, None] = False,
+    count: Union[int, None] = 1,
+):
+    logger.info(f"Request: [{ua=} {rf=} {hd=}]")
+    if count >= 250: count = 250
+    if count <= 0: count = 1
+    response = [masq(ua, rf, hd) for _ in range(count)]
     logger.info(f"Response: [{response}]")
 
     return JSONResponse(content=response)
